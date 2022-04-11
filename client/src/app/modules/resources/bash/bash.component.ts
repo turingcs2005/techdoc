7f3a1260f4f7ac4fbb90ad744d02f33fdfd07fef
Rui Wang <rui.wang.624@gmail.com>
2022-04-11 10:57:00 -0400

    minor edit

--- client/src/app/modules/resources/bash/bash.component.ts
+++ client/src/app/modules/resources/bash/bash.component.ts
@@ -16,7 +16,7 @@ export class BashComponent implements OnInit {
     new KeyBinding(2, 'mkdir -p ./dir1/dir2/', 'create a hierarchy of directories'),
     new KeyBinding(3, `find . -type d -name 'azure'\n find . -type f -name 'azure.ts'\n find . -type f -name '*.ts' -exec grep 'Emoji' {} \\;`, 
     'recursively find directory with name azure under the current directory\n recursively find file with name azure.ts under the current directory\n recursively find a .ts file containing string "Emoji" under the current directory'),
-    new KeyBinding(4, 'Ctrl + R; then type "ng serve"', 'reverse search command history for a command starting with "ng serve"'),
+    new KeyBinding(4, 'Ctrl r; then type "ng serve"', 'reverse search command history for a command starting with "ng serve"'),
     new KeyBinding(5, 'Ctrl Alt t', 'launch terminal'),
     new KeyBinding(6, 'Alt F2', 'launch quick terminal for one-liner, e.g. gnome-session-quit')
   ];
