cfe97236c9d68be0856256a346d25ab30e69f634
Rui Wang <rui.wang.624@gmail.com>
2022-04-12 12:49:00 -0400

    updated bash component

--- client/src/app/modules/resources/bash/bash.component.ts
+++ client/src/app/modules/resources/bash/bash.component.ts
@@ -17,8 +17,8 @@ export class BashComponent implements OnInit {
     new KeyBinding(3, `find . -type d -name 'azure'\n find . -type f -name 'azure.ts'\n find . -type f -name '*.ts' -exec grep 'Emoji' {} \\;`, 
     'recursively find directory with name azure under the current directory\n recursively find file with name azure.ts under the current directory\n recursively find a .ts file containing string "Emoji" under the current directory'),
     new KeyBinding(4, 'Ctrl r; then type "ng serve"', 'reverse search command history for a command starting with "ng serve"'),
-    new KeyBinding(5, 'Ctrl Alt t', 'launch terminal'),
-    new KeyBinding(6, 'Alt F2', 'launch quick terminal for one-liner, e.g. gnome-session-quit')
+    new KeyBinding(5, 'Ctrl Alt t', 'launch terminal', 'Ubuntu Linux shortcut'),
+    new KeyBinding(6, 'Alt F2', 'launch quick terminal for one-liner, e.g. gnome-session-quit', 'Ubuntu Linux shortcut')
   ];
 
   constructor() { }
