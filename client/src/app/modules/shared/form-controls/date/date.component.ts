b5dc11daf610b56e59a75c9b7e6d72195017d258
Rui Wang <rui.wang.624@gmail.com>
2022-06-28 16:09:25 -0400

    upgraded to Angular 14

--- client/src/app/modules/shared/form-controls/date/date.component.ts
+++ client/src/app/modules/shared/form-controls/date/date.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { UntypedFormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-date',
@@ -11,7 +11,7 @@ export class DateComponent implements OnInit {
   @Input() label = '';
   @Input() toolTip = '';
   @Input() size = 'medium-input';
-  @Input() control = new FormControl();
+  @Input() control = new UntypedFormControl();
   @Input() show = true;
 
   constructor() { }
